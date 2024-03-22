86665b52c5776e110f1f3d7c4193225f99372803
밍 <79096808+0321minji@users.noreply.github.com>
2024-03-22 20:38:18 +0900

    Update pgs_42840.py

--- 0321minji/pgs_42840.py
+++ 0321minji/pgs_42840.py
@@ -1,9 +1,11 @@
 def solution(answers):
     answer = []
+    #각 규칙 배열 선언
     one=[1,2,3,4,5]
     two=[2,1,2,3,2,4,2,5]
     three=[3,3,1,1,2,2,4,4,5,5]
     index1=index2=index3=0
+    #cnt dict으로 맞은 개수 관리
     cnt={1:0,2:0,3:0}
     for i in range(len(answers)):
         if answers[i]==one[index1]:
